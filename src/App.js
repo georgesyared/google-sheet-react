e05aa8ba3eb1423f7ce38cbe70e36e1d9922dc6f
Georges Yared <georges_yared99@hotmail.com>
2022-11-01 20:29:04 +0200

    Builds google-sheet and turn into a Rest Api

--- src/App.js
+++ src/App.js
@@ -1,56 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import { Button, TextField } from "@mui/material";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [tableData , setTableData] = useState([]);
+  const [totalSalary, setTotalSalary] = useState(0);
+
+  useEffect(()=>{
+      const total = tableData.reduce((total, row) => (total = total + Number(row.salary)),0);
+      setTotalSalary(total);
+  },[tableData])
+
+  useEffect(() => {
+      fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios
+    .get("https://sheet.best/api/sheets/e26e8d72-78d3-4d24-9e4e-8550990dd941")
+    .then((res) => {
+     console.log(res)
+     setTableData(res.data);
+    })
+  }
+
+
+  const submitFormToGoogle = (data) => {
+    axios
+      .post(
+        "https://sheet.best/api/sheets/e26e8d72-78d3-4d24-9e4e-8550990dd941",
+        data
+      )
+      .then((res) => {
+        alert("Row Successfully Added");
+        console.log(res);
+        setTableData([...tableData , data])
+      })
+      .catch((err) => alert(err.message));
+  };
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="contactForm">
+    
+         <table>
+         {tableData.map(({name,age,salary,hobby}) => (
+          <>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Salary</th>
+              <th>Hobby</th>
+            </tr>
+
+            <tr>
+              <td>{name}</td>
+              <td>{age}</td>
+              <td>{salary}</td>
+              <td>{hobby}</td>
+            </tr>
+
+            
+          </>
+          ))}
+         </table>
+
+         <h2>THE TOTAL SALARIES ${totalSalary}</h2>
+    
+
+      <form onSubmit={handleSubmit(submitFormToGoogle)}>
+        <TextField
+          name="name"
+          {...register("name", { required: true })}
+          error={errors.name}
+          helperText={errors.name && "The Name Is Required"}
+          label="Name"
+        />
+        <TextField
+          name="age"
+          {...register("age", { required: true })}
+          error={errors.age}
+          helperText={errors.age && "The Age Is Required"}
+          label="Age"
+        />
+        <TextField
+          name="salary"
+          {...register("salary", { required: true })}
+          error={errors.salary}
+          helperText={errors.salary && "The Salary Is Required"}
+          label="Salary"
+        />
+        <TextField
+          name="hobby"
+          {...register("hobby", { required: true })}
+          error={errors.hobby}
+          helperText={errors.hobby && "The Hobby Is Required"}
+          label="Hobby"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 }
